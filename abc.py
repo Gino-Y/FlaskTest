3ee94fb62f7f5c7dcd5027ddd5cc5edb5403e03a
Gino-Y <Gino-Y@users.noreply.github.com>
2020-08-26 14:35:12 +0800

    abc

---
+++ abc.py
@@ -0,0 +1,11 @@
+__author__ = 'Gino'
+# _*_ coding:utf-8 _*_
+# .@FileName :abc
+# .@Date....:2020-08-26 : 14 :34
+# .@Contact:yanjincai@foxmail.com
+'''
+launch :
+        import abc as gn_FileName
+        reload(FileName)
+        FileName.main()
+'''
