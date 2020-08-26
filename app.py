a848420cbe64f1c76c5f1a7f98867360f8a3b933
Gino-Y <Gino-Y@users.noreply.github.com>
2020-08-26 11:57:54 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import config
+
+# 创建一个flask对象
+app = Flask(__name__)
+app.config.from_object(config)
+
+
+@app.route('/')  # 装饰器
+def hello_world():
+    # a = 1
+    # b = 0
+    # c = a/b
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,12 @@
+__author__ = 'Gino'
+# _*_ coding:utf-8 _*_
+# .@FileName :config
+# .@Date....:2020-08-24 : 22 :41
+# .@Contact:yanjincai@foxmail.com
+'''
+launch :
+        import config as gn_FileName
+        reload(FileName)
+        FileName.main()
+'''
+DEBUG = False
